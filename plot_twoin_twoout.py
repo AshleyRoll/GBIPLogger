e116c7c4fb78b894a0beb3f019a935ae7580f791
Ashley Roll <ash@digitalnemesis.com>
2017-08-03 22:49:41 +1000

    Added temp-co plots and put plots into grid layout.

--- plot_twoin_twoout.py
+++ plot_twoin_twoout.py
@@ -7,11 +7,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.ticker import FuncFormatter
+
  
 
 sourceFile = sys.argv[1]
 
 avgWindow = 30		# 10 sec * 30 = 5 min average
+
+avgWindow = avgWindow * 2
 
 def make_patch_spines_invisible(ax):
     ax.set_frame_on(True)
@@ -25,6 +28,26 @@ def plot_series_and_average(axis, xSeries, mainSeries, avgSeries, color, name):
 	l = "%s SMA(%d)"%(name,avgWindow)
 	axis.plot(xSeries, avgSeries, color, label=l, zorder=2, alpha=1, linewidth=0.2)
 
+	
+def plot_temp_coefficient(axis, tempSeries, voltSeries, timeSeries, label):
+	axis.set_xlabel("°C")
+	axis.set_ylabel("Volts")
+	axis.set_title(label)
+	
+	axis.scatter(tempSeries, voltSeries, marker='.', linewidths=0.2, s=0.1, c=timeSeries, cmap='inferno')
+	
+	xmin = tempSeries.min()
+	xmax = tempSeries.max()
+	xfudge = (xmax - xmin) * 0.1
+	
+	ymin = voltSeries.min()
+	ymax = voltSeries.max()
+	yfudge = (ymax - ymin) * 0.1
+	
+	axis.set_xlim(xmin-xfudge, xmax+xfudge)
+	axis.set_ylim(ymin-yfudge, ymax+yfudge)
+	axis.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: ('%.7f')%x))
+	
 
 
 # import the CSV data
@@ -49,24 +72,34 @@ plt.rcParams['figure.dpi'] = 600
 plt.rcParams['savefig.dpi'] = 600
 plt.rcParams['figure.titlesize'] = 'small'
 
+# Create the basic axes 
+nrows = 6
+ncols = 2
 
-figure = plt.figure(1)
+figure = plt.figure(figsize=(7.5, 10.5))
 
+edcAxes = plt.subplot2grid( (nrows, ncols), (0, 0), rowspan=2, colspan=ncols )
+jvrAxes = plt.subplot2grid( (nrows, ncols), (2, 0), rowspan=2, colspan=ncols )
+tempAxes = plt.subplot2grid( (nrows, ncols), (4, 0), rowspan=1, colspan=ncols )
+edcTcAxes = plt.subplot2grid( (nrows, ncols), (5, 0), rowspan=1, colspan=1 )
+jvrTcAxes = plt.subplot2grid( (nrows, ncols), (5, 1), rowspan=1, colspan=1 )
+
+
+###############################################################################
 # EDC subplots
-laxis = plt.subplot(311)
-laxis.get_xaxis().set_visible(False)
+edcAxes.get_xaxis().set_visible(False)
 
 # plot left axis
+laxis = edcAxes
 laxis.set_ylabel("Volts")
 laxis.tick_params(axis='y', colors='b')
 plot_series_and_average(laxis, date_X, k2015a_edcV_Y, k2015a_edcV_Y_avg, "b-", "K2015_EDC")
 
 # plot right axis
-raxis = plt.twinx()
+raxis = laxis.twinx()
 raxis.set_ylabel("Volts")
 raxis.tick_params(axis='y', colors='r')
 plot_series_and_average(raxis, date_X, k196a_edcV_Y, k196a_edcV_Y_avg, "r-", "K196_EDC")
-
 
 laxis.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: ('%.7f')%x))
 raxis.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: ('%.5f')%x))
@@ -74,10 +107,10 @@ raxis.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: ('%.5f')%x))
 laxis.legend(loc=2)
 raxis.legend(loc=1)
 
-plt.tight_layout()
 
+###############################################################################
 # JFR subplots
-laxis = plt.subplot(312)
+laxis = jvrAxes
 laxis.get_xaxis().set_visible(False)
 
 # plot left axis
@@ -86,7 +119,7 @@ laxis.tick_params(axis='y', colors='b')
 plot_series_and_average(laxis, date_X, k2015a_jvrV_Y, k2015a_jvrV_Y_avg, "b-", "K2015_JVR")
 
 # plot right axis
-raxis = plt.twinx()
+raxis = laxis.twinx()
 raxis.set_ylabel("Volts")
 raxis.tick_params(axis='y', colors='r')
 plot_series_and_average(raxis, date_X, k196a_jvrV_Y, k196a_jvrV_Y_avg, "r-", "K196_JVR")
@@ -97,12 +130,9 @@ raxis.yaxis.set_major_formatter(FuncFormatter(lambda x, pos: ('%.5f')%x))
 laxis.legend(loc=2)
 raxis.legend(loc=1)
 
-plt.tight_layout()
-
-
+###############################################################################
 # temperature subplots
-laxis = plt.subplot(313)
-
+laxis = tempAxes
 
 # plot left axis
 roomTplot = laxis.plot(date_X, roomT_Y, "b-", label="Room Temp", linewidth=0.1)
@@ -110,9 +140,21 @@ windowTplot = laxis.plot(date_X, windowT_Y, "r-", label="Window Temp", linewidth
 		
 laxis.set_ylabel("°C")
 
-plt.legend()
+laxis.legend()
 
 plt.tight_layout()	
 
+###############################################################################
+# EDC Temp Co 
+plot_temp_coefficient(edcTcAxes, roomT_Y, k2015a_edcV_Y_avg, date_X, "EDC/K2015")
+
+
+############################################################################### 
+# JVR Temp Co 
+plot_temp_coefficient(jvrTcAxes, roomT_Y, k2015a_jvrV_Y_avg, date_X, "JVR/K2015")
+
+
+plt.tight_layout()
+
 #plt.show()
-figure.savefig(sourceFile + '.pdf', orientation='landscape', papertype='a4', bbox_inches='tight' )
+figure.savefig(sourceFile + '.pdf', format='pdf', orientation='portrait', papertype='a4' )
